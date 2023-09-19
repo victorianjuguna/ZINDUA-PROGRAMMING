caffa69d90cdf658e36a4101c1822545c932d645
Victoria Njuguna <victorianjerinjuguna@gmail.com>
2023-09-19 19:32:11 +0300

    added new file

---
+++ profit-calculator.py
@@ -0,0 +1,9 @@
+#create a file named profit-calculator.py
+#write a python program that calculates sales profit
+
+cost_price=200000
+selling_price=350000
+
+profit=selling_price-cost_price
+
+print (profit)
