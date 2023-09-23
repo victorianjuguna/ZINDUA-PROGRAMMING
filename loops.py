675c7f732e59ae88d952694e07dd04b69f50442d
Victoria Njuguna <victorianjerinjuguna@gmail.com>
2023-09-23 08:22:45 +0300

    added new files

---
+++ loops.py
@@ -0,0 +1,9 @@
+#while loops
+#boolean - true, false
+
+#for i in range(1,20):
+    #if (i %2==1):
+        #print(i, "is odd")
+    
+for num in range(0,1000,7):
+    print(num, "is divisible by 7")
